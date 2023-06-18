da96c10f9c1e7d9a86c8943b161d8baa53cee3a8
PhilRug <ruggiero.pj@gmail.com>
2023-06-18 19:05:48 -0400

    add user/ get singe user resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,9 +4,22 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    
+    // Get single user
+    me: async (_, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id})
+      }
+      throw new AuthenticationError('You need to be logged in!')
+    }
   },
   Mutation: {
+    // Create user
+    addUser: async (_, { username, email, password }, context) => {
+      const user = await User.create({ username, email, password});
+      const token = signToken(user);
+      return { user, token }
+    },
+    // Login
     
   },
 };

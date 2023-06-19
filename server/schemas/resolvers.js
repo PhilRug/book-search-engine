e12566f43f1801d5f81a6dd1d9875bd84997a738
PhilRug <ruggiero.pj@gmail.com>
2023-06-19 17:51:27 -0400

    save/delete book resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,7 @@ const resolvers = {
     // Get single user
     me: async (_, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id})
+        return User.findOne({ _id: context.user._id })
       }
       throw new AuthenticationError('You need to be logged in!')
     }
@@ -16,25 +16,52 @@ const resolvers = {
   Mutation: {
     // Create user
     addUser: async (_, { username, email, password }, context) => {
-      const user = await User.create({ username, email, password});
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { user, token }
     },
     // Login
     login: async (_, { email, password }, context) => {
       const user = await User.findOne({ email });
-        if (!user) {
-          throw new AuthenticationError ('No user with email found!');      
+      if (!user) {
+        throw new AuthenticationError('No user with email found!');
       }
       const validPassword = await bcrypt.compare(password, user.password);
-        if (!validPassword) {
-          throw new AuthenticationError('Incorrect password!');
+      if (!validPassword) {
+        throw new AuthenticationError('Incorrect password!');
       }
       const token = signToken(user);
       return { user, token };
     },
     // Save book
-    
+    saveBook: async (_, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    deleteBook: async (_, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        if (!updatedUser) {
+          return res.status(404).json({ message: "Couldn't find user with this id!" });
+        }
+        return (updatedUser);
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    }
   },
 };
 

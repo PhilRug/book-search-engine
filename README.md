fc4fea049840fa350d869b91611cf1774ef46276
PhilRug <ruggiero.pj@gmail.com>
2023-06-21 17:41:44 -0400

    searchBook/saveBook code updated

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 # book-search-engine
 a book search engine
 
+## License
 
+undefined
+## Description
+This search engine utilizes the Google Books API to fetch book data, which is then stored and managed in a MongoDB database. The backend is built with Node.js and Express, implementing a GraphQL API for efficient querying and manipulation of book information. The frontend is developed using React, providing a user-friendly interface for users to search, browse, and discover books from the extensive Google Books library.
+
+## Table of Contents
+    
+- [Installation](#installation)
+- [Usage](#usage)
+- [Testing](#test)
+- [Credits](#credits)
+    
+## Installation
+no installation needed
+
+## Usage
+Use through the web browser.
+* site found at 
+![front page]()
+
+## Testing
+download the repo and type
+`npm i` to install dependancies
+
+## Credits
+- starter code provided by https://github.com/coding-boot-camp/solid-broccoli.git
 - connection.js and server.js from lesson 21-MERN mini project 
+
+    
+## Questions
+If you have any questions you can contact us at:
+- Github https://github.com/PhilRug
+- Email ruggiero.pj@gmail.com
+
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,8 +8,11 @@ import {
   Row
 } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -17,9 +20,9 @@ const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -63,18 +66,18 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
       return false;
     }
-
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { input: bookToSave },
+      });
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error('something went wrong!');
       }
 
